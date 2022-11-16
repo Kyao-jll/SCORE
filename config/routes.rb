d48a1fdc96ea66442fd65bb10f23c6ee18a194dd
Kyao-jll <ke.yao@am.jll.com>
2022-11-16 09:01:14 +0000

    Register form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index"})
+  get("/register", { :controller => "users", :action => "register"})
+  get("/insert_user", { :controller => "users", :action => "create"})
 
 
 
